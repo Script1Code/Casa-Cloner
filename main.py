3511977b7bb0aa27990d0c9ddda6d684cafb161a
Script:Code <cristotempmail@gmail.com>
2024-10-13 22:04:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,8 +15,7 @@ def clear_console():
 
 
 def print_welcome_message():
-    print(Colorate.Horizontal(Colors.blue_to_white, Center.XCenter("""
-    
+    print(Colorate.Horizontal(Colors.blue_to_white, Center.XCenter("""    
 ░█████╗░░█████╗░░██████╗░█████╗░  ░█████╗░██╗░░░░░░█████╗░███╗░░██╗███████╗██████╗░
 ██╔══██╗██╔══██╗██╔════╝██╔══██╗  ██╔══██╗██║░░░░░██╔══██╗████╗░██║██╔════╝██╔══██╗
 ██║░░╚═╝███████║╚█████╗░███████║  ██║░░╚═╝██║░░░░░██║░░██║██╔██╗██║█████╗░░██████╔╝
@@ -50,24 +49,18 @@ async def delete_all_roles(guild):
 
 
 async def copy_server(source_guild, target_guild):
-    
     await target_guild.edit(name=source_guild.name)
 
-    
     if source_guild.icon is not None:
         icon_url = source_guild.icon.url if isinstance(source_guild.icon, discord.Asset) else None
         if icon_url:
             icon_data = await download_server_icon(icon_url)
             if icon_data:
                 await target_guild.edit(icon=icon_data)
-    
-    e
+
     await delete_all_channels(target_guild)
-    
-    
     await delete_all_roles(target_guild)
-    
-    
+
     for category in source_guild.categories:
         new_category = await target_guild.create_category(name=category.name, position=category.position)
         for channel in category.channels:
@@ -76,14 +69,12 @@ async def copy_server(source_guild, target_guild):
             elif isinstance(channel, discord.VoiceChannel):
                 await target_guild.create_voice_channel(name=channel.name, category=new_category, position=channel.position, bitrate=channel.bitrate, user_limit=channel.user_limit)
 
-    
     for channel in source_guild.channels:
         if isinstance(channel, discord.TextChannel) and channel.category is None:
             await target_guild.create_text_channel(name=channel.name, position=channel.position, topic=channel.topic, nsfw=channel.nsfw, slowmode_delay=channel.slowmode_delay)
         elif isinstance(channel, discord.VoiceChannel) and channel.category is None:
             await target_guild.create_voice_channel(name=channel.name, position=channel.position, bitrate=channel.bitrate, user_limit=channel.user_limit)
 
-    
     for role in source_guild.roles[::-1]:  
         if role.name != "@everyone":  
             try:
@@ -101,8 +92,7 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 async def start_cloning():
     clear_console()  
     print_welcome_message()
-    
-    
+
     source_guild_id = input(Colorate.Horizontal(Colors.red_to_yellow, "Inserisci l'ID del server da copiare: "))
     target_guild_id = input(Colorate.Horizontal(Colors.red_to_yellow, "Inserisci l'ID del server di destinazione: "))
 
@@ -133,13 +123,11 @@ async def start_cloning():
 async def on_ready():
     await start_cloning()  
 
-
 if __name__ == "__main__":
     clear_console()  
     print_welcome_message()
     token = input(Colorate.Horizontal(Colors.blue_to_purple, "Inserisci il token del bot: "))
     bot.run(token)
-
 
 
 
